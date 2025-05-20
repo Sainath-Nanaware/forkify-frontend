5f7debffc1364fe6a49b476e7f1aa2a8d23260a1
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-05-20 18:46:31 +0530

    complete header with logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-   <header className='pb h-[10vh] bg-gray-80 flex justify-center items-center'>
+   <header className='  h-[10vh] bg-gray-80 flex justify-center items-center bg-[#F5F5F5]'>
         {/* logo */}
-        <div className='pb w-[50%] h-[100%] pl-[10vw]'>
-            <div className='pb w-[37vw] h-[100%] bg-[#282828]
- flex justify-center items-center'>
+        <div className=' w-[50%] h-[100%] pl-[10vw]'>
+            <div className=' w-[36.4vw] h-[100%] bg-[#fdd677]
+ flex justify-center items-center pl-[3vw]'>
+                    <Link to={"/"}>
                     <svg width="260" height="74" viewBox="0 0 260 100" xmlns="http://www.w3.org/2000/svg" fill="none">
                         <g transform="translate(10, 15)">
-                          <rect x="10" y="5" width="4" height="30" fill="#ffffff" />
-                          <rect x="16" y="5" width="4" height="30" fill="#ffffff" />
-                          <rect x="22" y="5" width="4" height="30" fill="#ffffff" />
-                          <rect x="14" y="35" width="8" height="25" rx="2" fill="#ffffff" />
+                          <rect x="10" y="5" width="4" height="30" fill="#4B5563" />
+                          <rect x="16" y="5" width="4" height="30" fill="#4B5563" />
+                          <rect x="22" y="5" width="4" height="30" fill="#4B5563" />
+                          <rect x="14" y="35" width="8" height="25" rx="2" fill="#6B7280" />
                         </g>
 
-                        <text x="50" y="70" font-family="Segoe UI, Helvetica, Arial, sans-serif" font-size="36" fill="#ffffff" font-weight="600" letter-spacing="1">
+                        <text x="50" y="70" font-family="Segoe UI, Helvetica, Arial, sans-serif" font-size="36" fill="#111827" font-weight="600" letter-spacing="1">
                           Forkify
                         </text>
                      </svg>
-
+                    </Link>
             </div>
 
         </div>
         {/* login */}
-        <div className='pb w-[50%] h-[100%]'>
+        <div className='w-[50%] h-[100%] flex justify-end items-center gap-10 pr-[5vw]'>
+          <Link className='font-semibold hover:text-[#FEBE10]'>Home</Link>
+          <Link className='font-semibold hover:text-[#FEBE10]'>About Us</Link>
+          <Link className='font-semibold hover:text-[#FEBE10]'>Blog</Link>
+          <Link className='font-semibold hover:text-[#FEBE10]'>Login</Link>
+          <Link className='font-semibold hover:text-[#FEBE10]'>Sign up</Link>
 
         </div>
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import imageBiryani from "../assets/Chicken_biryani_banner.png"
+import thaliDish from "../assets/Thali_food_platter.jpg"
 function Main() {
   return (
-    <main className='pb h-[80vh]'>
+    <main className=' h-[180vh] flex justify-center items-center bg-[#F5F5F5]'>
+      {/* image portion */}
+      <div className=' h-[100%] w-[50%] pl-[10vw]'>
+        <div>
+          <img src={thaliDish} alt="dishImage" className='h-[100vh] ' />
+        </div>
 
+      </div>
+      {/* text portion */}
+      <div className=' h-[100%] w-[50%]'>
+
+
+      </div>
 
 
     </main>
